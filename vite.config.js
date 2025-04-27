6c91d2cbdf32adc39e843863ff8bd9e2f04d21cb
Ahmed Bakry <aahmedbakry654@gmail.com>
2025-04-27 21:28:10 +0300

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Zaza-photographer/', // << أضف السطر ده
+  base: '/',
   plugins: [react()],
 })
