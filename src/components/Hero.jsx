fae2cf48b91e9c027fc667ef83bf0fcb52c06e8d
Ahmed Bakry <aahmedbakry654@gmail.com>
2025-04-28 22:20:39 +0300

    Fix video playback for iPhone - added playsInline

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,14 +11,17 @@ const Hero = () => {
         loop
         muted
         playsInline
-      />
-      
+        preload="auto" // دي مهمة عشان يبدأ يحمل الفيديو قبل العرض
+      >
+        Your browser does not support the video tag.
+      </video>
+
       <div className="hero-content">
         <h1 className="decorated-text">Zaza Photographer</h1>
         <p className="subscript">Welcome to Zaza Photographer Page.</p>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
