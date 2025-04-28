1ec31b5b56134570253cc7316b4f97cc4f275eec
Ahmed Bakry <aahmedbakry654@gmail.com>
2025-04-28 22:07:12 +0300

    Fix video playback for iPhone - added playsInline

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,21 @@ import './Hero.css';
 const Hero = () => {
   return (
     <div className="hero">
-      <video className="hero-video" autoPlay loop muted>
-        <source src="/videos/vedio2.mp4" type="video/mp4" />
-      </video>
+      <video
+        className="hero-video"
+        src="/videos/vedio2.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+      
       <div className="hero-content">
         <h1 className="decorated-text">Zaza Photographer</h1>
         <p className="subscript">Welcome to Zaza Photographer Page.</p>
       </div>
     </div>
   );
-};
+}
 
 export default Hero;
